ac6bc0fbcd025521fa06314dd8a662e4dc33aa65
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-02-13 13:36:36 +0100

    Debugging

--- lib/player.rb
+++ lib/player.rb
@@ -15,13 +15,13 @@ class Player
   end
 
   def select_name
-    print player_notification(:get_name)
+    print player_notification[:get_name]
     gets.chomp
-    puts player_notification(:greet_player)
+    puts player_notification[:greet_player]
   end
 
   def select_move
-    print player_notification(:select_move)
+    print player_notification[:select_move]
     gets.chomp
   end
 end
