0ee69deb719a1eca934066af3593587eec91e8e1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-02-13 12:40:20 +0100

    Minor changes

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -7,12 +7,12 @@ module Notifications
   player_notification = {
     select_name: "Type in name for Player#{@player_number}: ",
     select_move: "Player#{@player_number}(#{@name}) Select your column: ",
-    greet_player: "Hi #{@name}!",
-    column_full: 'This column is full! Pick another move'
+    greet_player: "Hi #{@name}!"
   }
 
   game_notification = {
     selection_error: "Your selection is invalid, please select a column
-    between 1 and #{board.width}"
+    between 1 and #{board.width}",
+    column_full: 'This column is full! Pick another move'
   }
 end
