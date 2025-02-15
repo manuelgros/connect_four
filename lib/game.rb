e63eabc4ae098eb426a88a5f9ff4420438245036
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-02-15 13:31:15 +0100

    Re-factor code for efficiency and readability

--- lib/game.rb
+++ lib/game.rb
@@ -8,14 +8,16 @@ require './lib/notifications'
 class Game
   include Notifications
 
+  attr_reader :current_player, :board, :player_one, :player_two
+
   def initialize
-    @player_one = Player.new
-    @player_two = Player.new
     @board = GameBoard.new
+    @player_one = Player.new(board.white_tile)
+    @player_two = Player.new(board.black_tile)
     @current_player = player_one
   end
 
-  def player_selection(player)
+  def getting_player_selection(player)
     loop do
       selection = player.select_move
       break selection.to_i if board.column_exist?(selection) && selection.match?(/\A\d+\Z/)
@@ -24,21 +26,48 @@ class Game
     end
   end
 
-  def check_selection(selection)
-    selected_column = board.select_column(selection)
+  def next_player
+    @current_player = current_player == player_one ? player_two : player_one
+  end
+
+  def execute_move(player)
+    selected_column = getting_player_selection(player)
     if board.column_full?(selected_column)
       puts game_notification[:column_full]
-      player_selection(current_player)
+      execute_move(player)
     else
-      selected_column
+      check_column(selected_column)
+      board.update_position(board.find_position(selected_column), player.marker)
     end
   end
 
-  def execute_move(selected_column)
-    board.update_position(board.find_position(selected_column), current_player.marker)
+  def check_for_win?
+    # neighbors = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+    directions = [[0, 1], [1, 1], [1, 0]]
+    @coordinates.each do |start_position|
+      directions.each { |direction| four_connected?(start_position, direction) }
+    end
   end
 
-  def next_player
-    @current_player = current_player == player_one ? player_two : player_one
+  def four_connected?(start_position, direction, streak = 1)
+    return true if streak == 4
+
+    neighbor = start_position.map.with_index { |num, idx| num + direction[idx] }
+
+    if board[start_position] == board[neighbor]
+      four_connected?(neighbor, direction, streak + 1)
+    else
+      false
+    end
+  end
+
+  def play_full_round
+    until check_for_win? || board.board_full?
+      execute_move(@current_player)
+      next_player
+    end
+    return puts game_notification[:game_over_win] if check_for_win?
+
+    puts game_notification[:game_over_full]
   end
 end
