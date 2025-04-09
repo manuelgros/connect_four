cce0c419dad11507e28a1213d42dcda3bb10c2bf
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-09 14:19:14 -0700

    Updated to last stand after losing filw

--- lib/player.rb
+++ lib/player.rb
@@ -1,27 +1,28 @@
 # frozen_string_literal: true
 
-require './lib/notifications'
+require_relative '../lib/notifications'
 
 # Player class
 class Player
-  attr_accessor :player_number
-
   include Notifications
 
-  def initialize(marker)
-    @name = select_name
-    @player_number = nil
+  attr_accessor :player_number
+  attr_reader :marker
+
+  def initialize(player_number, marker)
+    @player_number = player_number
     @marker = marker
+    @name = select_name
+    puts player_notification(:greet_player)
   end
 
   def select_name
-    print player_notification[:get_name]
+    print player_notification(:select_name)
     gets.chomp
-    puts player_notification[:greet_player]
   end
 
   def select_move
-    print player_notification[:select_move]
+    print player_notification(:select_move)
     gets.chomp
   end
 end
